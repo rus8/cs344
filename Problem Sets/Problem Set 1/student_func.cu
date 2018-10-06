da6ade2c55787bb71bfdda06fb42f6e524af0ef5
Ruslan Aminev <amirusrif@gmail.com>
2018-10-06 12:20:02 +0200

    Solution for Problem 1

--- Problem Sets/Problem Set 1/student_func.cu
+++ Problem Sets/Problem Set 1/student_func.cu
@@ -43,13 +43,20 @@ void rgba_to_greyscale(const uchar4* const rgbaImage,
   //the mapping from components of a uchar4 to RGBA is:
   // .x -> R ; .y -> G ; .z -> B ; .w -> A
   //
+  int x_pos = threadIdx.x; // thread in block
+  int y_pos = blockIdx.x;  // block in grid
   //The output (greyImage) at each pixel should be the result of
   //applying the formula: output = .299f * R + .587f * G + .114f * B;
   //Note: We will be ignoring the alpha channel for this conversion
-
+  if (y_pos * numCols + x_pos > numRows * numCols - 1){
+      return;
+  }
   //First create a mapping from the 2D block and grid locations
   //to an absolute 2D location in the image, then use that to
   //calculate a 1D offset
+  int abs_pos = y_pos * numCols + x_pos;
+  greyImage[abs_pos] = .299f * rgbaImage[abs_pos].x + .587f * rgbaImage[abs_pos].y + .114f * rgbaImage[abs_pos].z;
+
 }
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
@@ -57,10 +64,10 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(1, 1, 1);  //TODO
-  const dim3 gridSize( 1, 1, 1);  //TODO
+  const dim3 blockSize(numCols, 1, 1);  //number of threads
+  const dim3 gridSize(numRows, 1, 1);  //number of blocks
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
-  
+
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 
 }
